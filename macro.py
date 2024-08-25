4f0bce46c1e2d0b885153bcb1ff15be46f1771e0
Artyom-Murzinov <murzinov_a@bk.ru>
2024-08-25 07:27:38 +0500

    Update macro.py
    добавил многопользовательское использование

--- macro.py
+++ macro.py
@@ -16,19 +16,28 @@ word_3 = ["Введите ширину кармана: ", "Введите дли
           "Поверхность детали: ", "Начальный угол: ", "Средний радиус паза: ", "Введите радиус фрезы: ", 
           "Введите шаг фрезерования: ", "Коэффициент перекрытия фрезы: "]
 word_4 = ['Введите скорость Vc', 'Введите диаметр фрезы D', 'Введите число зубев Z', 'Введите съем на зуб Fz']
-number_dict = {'number': 0}
-conditions = {'kelb': list}
-text_nc = {"text": "% \nO0001 \n"}
-working_dict = {'list': []}
+user = {}
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.number_dict = {'number': 0}
+        self.conditions = {'kelb': []}
+        self.text_nc = {"text": "% \nO0001 \n"}
+        self.working_dict = {'list': []}
+
 
 
 def mathematics(number_list):
-    n = (1000 * float(number_list[0]))/(3.14 * float(number_list[1]))
-    f = float(number_list[2]) * float(number_list[3]) * n
-    return int(n), int(f)
+    try:
+        n = (1000 * float(number_list[0]))/(3.14 * float(number_list[1]))
+        f = float(number_list[2]) * float(number_list[3]) * n
+        return int(n), int(f)
+    except:
+        return 'отправьте end, и выберите цикл', None
 
 
-def generating_file(action):
+def generating_file(action, user_id):
     if action == word:
         action1 = "KK"   
     elif action == word_2:
@@ -39,11 +48,11 @@ def generating_file(action):
         new_code = open("O0001.nc", "w")
         with open(f"{action1.lower()}.txt", "r", encoding="utf-8") as pk:
             info = pk.read()
-        new_code.write(text_nc['text'])
+        new_code.write(user[user_id].text_nc['text'])
         with open("O0001.nc", "a") as new_code:
             new_code.write(info)
-        text_nc["text"] += info
-        return text_nc["text"]
+        user[user_id].text_nc["text"] += info
+        return user[user_id].text_nc["text"]
 
 
 def func_error(argumet, numb):
@@ -72,6 +81,8 @@ def gen_markup():
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
+    user_id = message.from_user.id 
+    user[user_id] = User(user_id)
     bot.send_message(
         message.from_user.id,
         "Выберите нужный цикл!",
@@ -83,66 +94,75 @@ def func_generato():
     '''Заполняю текстовый документ нужными параметрами'''
     @bot.message_handler(func=lambda message: True)
     def echo_all(message):
-        print(message.text)
-        if message.text == 'end':
-            bot.send_message(message.chat.id, 'Прерываю цикл!')
+        user_id = message.from_user.id 
+        if message.text.lower() == 'end':
+            bot.send_message(message.from_user.id, 'Прерываю цикл!')
             return
-        number, erorrs = func_error(message.text, number_dict["number"])
-        number_dict["number"] = number
+        number, erorrs = func_error(message.text, user[user_id].number_dict["number"])
+        user[user_id].number_dict["number"] = number
         if erorrs != None:
-            bot.send_message(message.chat.id, erorrs)
-        if number <= len(conditions['kelb']) -1:
-            bot.send_message(message.chat.id, conditions['kelb'][number])
+            bot.send_message(message.from_user.id, erorrs)
+        if number <= len(user[user_id].conditions['kelb']) -1:
+            bot.send_message(message.from_user.id, user[user_id].conditions['kelb'][number])
     ##############################################################################
-        if conditions['kelb'] == word or conditions['kelb'] == word_2 or conditions['kelb'] == word_3:
+        if user[user_id].conditions['kelb'] == word or user[user_id].conditions['kelb'] == word_2 or user[user_id].conditions['kelb'] == word_3:
             '''Условие для записи G-кода'''
             if number > 0:
-                text_nc["text"] += f"#{number} = {message.text}\n"
-            if number >= len(conditions["kelb"]):
-                gen_text = generating_file(conditions['kelb'])
-                bot.send_message(message.chat.id, gen_text) 
-                text_nc["text"] = "% \nO0001 \n" 
-        elif conditions['kelb'] == word_4:
+                user[user_id].text_nc["text"] += f"#{number} = {message.text}\n"
+            if number >= len(user[user_id].conditions["kelb"]):
+                gen_text = generating_file(user[user_id].conditions['kelb'], user_id)
+                bot.send_message(message.from_user.id, gen_text) 
+                return
+        elif user[user_id].conditions['kelb'] == word_4:
             '''Условие деля записи режимов резания'''
             if number > 0:
-                working_dict["list"].append(message.text)
-            if number >= len(conditions["kelb"]):
-                turnovers, supply = mathematics(working_dict["list"])
-                bot.send_message(message.chat.id, f'число оборотов = {turnovers} \nПодача = {supply}') 
-                working_dict["list"]= [] 
-        return
-    number_dict["number"] = 0        
+                user[user_id].working_dict["list"].append(message.text)
+            if number >= len(user[user_id].conditions["kelb"]):
+                turnovers, supply = mathematics(user[user_id].working_dict["list"])
+                if supply != None:
+                    bot.send_message(message.from_user.id, f'число оборотов = {turnovers} \nПодача = {supply}')
+                else:
+                    bot.send_message(message.from_user.id, turnovers)
+                return       
     return
 
 # Генераторы циклов!!!
 @bot.message_handler(func=lambda message: message.text == "Круглый карман")
 def echo_all(message):
-    bot.send_message(message.chat.id, 'Отправьте любую БУКВУ')
-    conditions['kelb'] = word   
+    user_id = message.from_user.id
+    user[user_id] = User(user_id) 
+    bot.send_message(message.from_user.id, 'Отправьте любую БУКВУ')
+    user[user_id].conditions['kelb'] = word   
     func_generato()    
     return
 
 
 @bot.message_handler(func=lambda message: message.text == "Прямоугольный карман")
 def echo_all(message):
-    bot.send_message(message.chat.id, 'Отправьте любую БУКВУ')
-    conditions['kelb'] = word_2
+    user_id = message.from_user.id
+    user[user_id] = User(user_id) 
+    bot.send_message(message.from_user.id, 'Отправьте любую БУКВУ')
+    user[user_id].conditions['kelb'] = word_2
     func_generato()
     return
 
 
 @bot.message_handler(func=lambda message: message.text == "Радиусный карман")
 def echo_all(message):
-    bot.send_message(message.chat.id, 'Отправьте любую БУКВУ')
-    conditions['kelb'] = word_3
+    user_id = message.from_user.id
+    user[user_id] = User(user_id) 
+    bot.send_message(message.from_user.id, 'Отправьте любую БУКВУ')
+    user[user_id].conditions['kelb'] = word_3
     func_generato()
     return
 
 
 @bot.message_handler(func=lambda message: message.text == "Расчет режимов резания")
 def echo_all(message):
-    bot.send_message(message.chat.id, 'Отправьте любую БУКВУ')
-    conditions['kelb'] = word_4
+    user_id = message.from_user.id
+    user[user_id] = User(user_id) 
+    bot.send_message(message.from_user.id, 'Отправьте любую БУКВУ')
+    user[user_id].conditions['kelb'] = word_4
     func_generato()
     return
 
