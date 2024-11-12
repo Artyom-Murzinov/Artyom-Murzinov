aa5a01a5115a893f4cf2c588c810b8655e8d06ff
Artyom-Murzinov <murzinov_a@bk.ru>
2024-11-13 00:04:49 +0500

    Update inline.py

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -2,13 +2,17 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardMarkup
 
 
 def cycles() -> InlineKeyboardMarkup:
-    """клавиатура Фрезерные циклы"""
+    """клавиатура Фрезерные циклы карманов"""
     kb = InlineKeyboardBuilder()
     kb.button(text="Круглый карман", callback_data="round_pocket")
+    kb.button(text="Чистовой круглый карман", callback_data="finishing_round_pocket")
     kb.button(text="Радиусный карман", callback_data="radius_pocket")
     kb.button(text="Прямоугольный карман", callback_data="rectangular_pocket")
+    kb.button(text="Чистовой прямоугольный карман", callback_data="finishing_rectangular_pocket")
     kb.button(text="Фрезерование плоскости", callback_data="milling _plane")
+    kb.button(text="Фрезерование внутренней резьбы", callback_data="thread_milling")
     kb.button(text="Коническая резьба", callback_data="conical_thread")
+#    kb.button(text="Гравировка текста", callback_data="engraving")
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True)
 
@@ -18,7 +22,7 @@ def calculator() -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     kb.button(text="Расчет режимов резания", callback_data="cutting_mode")
     kb.button(text="Превод в десятичный угол", callback_data="degrees_decimal")
-    kb.button(text="Фрезерная поворотная голова UFG", callback_data="UFG")
+    kb.button(text="Фрезерная поворотная UFG", callback_data="UFG")
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True)
 
