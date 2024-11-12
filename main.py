25db9c6b056e1cfa2dd88ff791aec53ad73f128b
Artyom-Murzinov <murzinov_a@bk.ru>
2024-11-13 00:00:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 
-
 async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
@@ -18,13 +17,15 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     while True:
         try:
             print("Бот запущен!!!")
             asyncio.run(main())
         except:
-            sleep(1)
+            sleep(0.3)
+
 
 
 
