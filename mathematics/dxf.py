305eda34df151ffacfff105d67153cd9b60ad01e
Артём <murzinov_a@bk.ru>
2024-11-22 01:16:57 +0500

    Update dxf.py

--- mathematics/dxf.py
+++ mathematics/dxf.py
@@ -1,31 +1,47 @@
 import ezdxf
-import pathlib
-from ezdxf.addons.text2path import Kind, explode, virtual_entities
+from ezdxf.addons.text2path import Kind, explode
 from ezdxf.enums import TextEntityAlignment
 import math
 from ezdxf.addons.drawing import matplotlib
 import configparser
+from ezdxf.fonts import fonts
 config = configparser.ConfigParser()
 config.read("settings.ini")
-ezdxf.options.support_dirs.append(config['core']["support_dirs"])
+ezdxf.options.support_dirs = config['core']["support_dirs"]
+fonts.build_system_font_cache()
 
 
 class TextEngraving:
     def __init__(self, dictory):
+        list_shrift = ["ГОСТ тип А.ttf", "T-FlexA.ttf", "AmericanTypewriter.ttc", "PTSans.ttc", "GOST 26.008-85.ttf"]
+        self.fonts = list_shrift[int(dictory["fonts"])]
+        self.text_position = (float(dictory["text_position"].split(',')[0]), float(dictory["text_position"].split(',')[1]))  #расположение в системе координат
+        if int(dictory["text_alignments"]) == 1:
+            self.location = TextEntityAlignment.BOTTOM_LEFT
+        if int(dictory["text_alignments"]) == 2:
+            self.location = TextEntityAlignment.TOP_LEFT
+        if int(dictory["text_alignments"]) == 3:
+            self.location = TextEntityAlignment.TOP_CENTER
+        if int(dictory["text_alignments"]) == 4:
+            self.location = TextEntityAlignment.TOP_RIGHT
+        if int(dictory["text_alignments"]) == 5:
+            self.location = TextEntityAlignment.BOTTOM_RIGHT
+        if int(dictory["text_alignments"]) == 6:
+            self.location = TextEntityAlignment.BOTTOM_CENTER
+
         self.doc = ezdxf.new("AC1032", setup=True)
         self.msp = self.doc.modelspace()
         self.text_circle = dictory["text_circle"] #прямое или по окружности
         self.text_content = dictory["text_content"] #текст
-        self.text_size = dictory["text_size"]        #высота символов
-        self.shag = dictory["shag"]             #между символами
-        self.text_position = dictory["text_position"] #расположение в системе координат
-        self.angle = dictory["angle"] #угол
-        self.fonts = dictory["fonts"] #Шрифт
+        self.text_size = float(dictory["text_size"])        #высота символов
+        self.shag = float(dictory["shag"])             #между символами
+        self.angle = float(dictory["angle"]) #угол
+        self.oblique = float(dictory['oblique']) # наклон
+        #FONT = fonts.FontFace(family=self.fonts)
         # Радиус окружности
         if dictory["text_circle"] == 1:
             self.radius = dictory["radius"]           #радиус 
-        self.doc.styles.add("myStandard", font = self.fonts)
-
+        self.doc.styles.new("myStandard", dxfattribs = {'font': self.fonts})
 
 
     def __str__(self):
@@ -59,8 +75,9 @@ class TextEngraving:
             dxf_poli = self.msp.add_text(self.text_content, rotation=self.angle, dxfattribs = {
                 'height': self.text_size,
                 'style': "myStandard",
-                'width': self.shag
-            }).set_placement(self.text_position, align=TextEntityAlignment.MIDDLE_CENTER)
+                'width': self.shag,
+                'oblique': 15
+            }).set_placement(self.text_position)
             text = TextEngraving.func_grav(dxf_poli, text)
         if self.text_circle == 1:    
             num_chars = len(self.text_content)
@@ -75,8 +92,8 @@ class TextEngraving:
                 x = self.text_position[0] + self.radius * math.cos(math.radians(self.angle))  # Вычисляем x-координату
                 y = self.text_position[1] + self.radius * math.sin(math.radians(self.angle))  # Вычисляем y-координату
                 self.msp.add_text(char, rotation=self.angle-90, dxfattribs = {
-                'height': self.text_size,
-                'style' : "myStandard"}).set_placement((x, y), align=TextEntityAlignment.CENTER)
+                    'height': self.text_size,
+                    'style' : "myStandard"}).set_placement((x, y), align = TextEntityAlignment.CENTER)
                 self.angle -= angle_step 
             for dxf_poli in self.msp:
                 if dxf_poli.dxftype() =="TEXT": 
@@ -84,24 +101,3 @@ class TextEngraving:
         matplotlib.qsave(self.doc.modelspace(), 'your.png')
         text += f"G0 Z200 \nM30 \n%"
         return text
-
-
-list_shrift = ["T-FlexA.ttf", "ГОСТ ТИП А.TTF", 
-               "ГОСТ ТИП А НАКЛОННЫЙ.TTF", 
-               "txt_____.ttf", "GOST_AU.shx", "GOST_A.TTF",
-               "txt.shx", "AmericanTypewriter.ttc"]
-
-dictonari = {"text_content":"Привет как-бы я MacroGcode", "text_circle": 0, "text_size":3,
-           "text_position":(0, 0), "angle": 0, "shag": 1, "radius":20, "fonts": list_shrift[0]}
-
-
-
-user = TextEngraving(dictonari)
-with open("O0001.nc", "w") as file:
-    file.write(str(user))
-
-
-if __name__ == "__main__":
-    user = TextEngraving(dictonari)
-    with open("O0001.nc", "w") as file:
-        file.write(str(user))
